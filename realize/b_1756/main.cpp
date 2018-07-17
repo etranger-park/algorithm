975e552ecf03887ba6b41caa530297750e42f045
Suho Park <hoo3302@gmail.com>
2018-07-18 02:21:07 +0900

    Baekjoon realize-2
    Baekjoon realize 6581 try one more again

--- realize/b_1756/main.cpp
+++ realize/b_1756/main.cpp
@@ -7,81 +7,76 @@
 using namespace std;
 
 map<int,int> min_map; //(min -> locate)
-set<int> min_set;//The keys set
 
 int up(int a)
 {
     int h,new_val;
     map<int,int>::iterator it_map;
     set<int>::iterator it_set;
-    if(a <= *(min_set.begin())) it_set = min_set.begin();
+    if(a<= min_map.begin()->first) it_map = min_map.begin();
     else
     {
-        for(it_set = min_set.end();it_set != min_set.begin();)
+        for(it_map = min_map.end();it_map != min_map.begin();)
         {
-            it_set--;
-            int d_r = *(it_set);
+            it_map--;
+            int d_r = it_map->first;
             if(d_r == a) break;
             else if(d_r > a) continue;
             else
             {
-                it_set++;
+                it_map++;
                 break;
             }
         }
     }
-    h = min_map[*(it_set)];
-    //printf("In %d, pick height %d, radius %d\n",a,h,*(it_set));
-    if(it_set != min_set.begin())
-        min_set.erase(min_set.begin(),it_set);
-    new_val = min_map[*(it_set)];
-    min_map[*(it_set)] = --new_val;
+    h = it_map -> second;
 
-    if(++it_set == min_set.end()) return -1;
+    if(it_map != min_map.begin())
+        min_map.erase(min_map.begin(),it_map);
 
-    if(min_map[*it_set] == new_val)
+    new_val = h;
+    min_map[it_map -> first] = --new_val;
+
+    if(++it_map == min_map.end()) return -1;
+
+    if(it_map->first == new_val)
     {
      //아래부터 쌓아 올려서 해당 폭을 가지는 애가 가득 찼을 때
-        min_set.erase(--it_set);
+        min_map.erase(--it_map);
     }
     return h;
 }
 
 int main()
 {
-    int d, n,i,max_h,min_val = 2000000000;
+    int radius,d, n,i,max_h,min_val = 2000000000;
     scanf("%d %d",&d,&n);
-    int *d_radius = (int *) malloc(sizeof(int)*d);
     int *d_min = (int *) malloc(sizeof(int)*d);
-    int *n_radius = (int *) malloc(sizeof(int)*n);
     map<int,int>::iterator it_map;
-    set<int>::iterator it_set;
 
     for(i=0;i<d;i++)
     {
-        scanf("%d",d_radius + i);
-        if(min_val > *(d_radius + i))
+        scanf("%d",&radius);
+        if(min_val > radius)
         {
             min_map.insert(pair<int,int>(min_val,i-1));
-            min_set.insert(min_val);
-            min_val = *(d_radius + i);
+            min_val = radius;
 
         }
         *(d_min + i) = min_val;
     }
 
     min_map.insert(pair<int,int>(min_val,i-1)); // insert the last min value
-    min_set.insert(min_val);
-    /*for(it_map=min_map.begin();it_map!=min_map.end();it_map++)
+    /*
+    for(it_map =min_map.begin();it_map!=min_map.end();it_map++)
     {
-        printf("%d:%d\n",it_map->first,it_map->second);
+        printf("min: %d -> locate: %d\n",it_map->first,it_map->second);
     }*/
 
     for(i=0;i<n;i++)
     {
-        scanf("%d",n_radius + i);
-        int n_val = *(n_radius + i);
-        max_h = up(n_val);
+        scanf("%d",&radius);
+        max_h = up(radius);
         if(max_h == -1)
         {
             printf("0");
---
+++ realize/b_6581/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+void handling_br()
+{
+
+}
+
+int main()
+{
+    while(true)
+    return 0;
+}
